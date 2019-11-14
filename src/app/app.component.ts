6dab53515aca3a311da9080434c7dd0a5ba16b85
Julien Moratalla <jmo@connecthings.com>
2019-11-14 17:05:59 +0100

    add responsive menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'poc-angular8';
+
+  ngOnInit() {
+    $(document).ready(function() {
+      $("#menu").click(function(){
+        $(".mobile-nav").toggle(200);
+      });
+    });
+  };
+
+
 
 }
